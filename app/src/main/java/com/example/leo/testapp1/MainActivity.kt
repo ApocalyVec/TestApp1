fe3f921499114f5c0186e50c3a4040b96853d471
ApocalyVec <zli12@wpi.edu>
2018-11-01 16:41:46 -0400

    Initial commit

---
+++ app/src/main/java/com/example/leo/testapp1/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.leo.testapp1
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+const val EXTRA_MESSAGE = "com.example.TestApp1.MESSAGE"
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    /* call when the user taps the send button */
+    fun sendMessage(view: View) {
+        val editText = findViewById<EditText>(R.id.editText)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent);
+    }
+}
